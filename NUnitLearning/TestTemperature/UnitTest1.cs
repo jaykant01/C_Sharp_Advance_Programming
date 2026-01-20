38dfbccbe3c7c884e30f6fbc08a3013b36979c8a
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 20:49:31 +0530

    [Jaykant] Add .Temperature Converter NUnit Testing

---
+++ NUnitLearning/TestTemperature/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿namespace TestTemperature;
+
+[TestFixture]
+public class Tests
+{
+    private TemperatureConverter temperatureConverter;
+    [SetUp]
+    public void Setup()
+    {
+        temperatureConverter = new TemperatureConverter();
+    }
+
+    [TestCase(0, 32)]
+    [TestCase(100, 212)]
+    [TestCase(37, 98.6)]
+    [TestCase(-40, -40)]
+    public void CelsiusToFahrenheit_ReturnCorrectValue(double celsius, double expectedFahr)
+    {
+        double result = temperatureConverter.CelsiusToFahrenheit(celsius);
+        
+        Assert.That(result, Is.EqualTo(expectedFahr).Within(0.1));
+    }
+
+
+    [TestCase(32, 0)]
+        [TestCase(212, 100)]
+        [TestCase(98.6, 37)]
+        [TestCase(-40, -40)]
+        public void FahrenheitToCelsius_ShouldReturnCorrectValue(double fahrenheit, double expectedCelsius)
+        {
+            // Act
+            double result = temperatureConverter.FahrenheitToCelsius(fahrenheit);
+
+            // Assert 
+            Assert.That(result, Is.EqualTo(expectedCelsius).Within(0.1));
+        }
+}
