f94dc87095841b9912fa7816f26a9b113916b9c6
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 22:00:08 +0530

    [Jaykant] Add .User Registration NUnit Testing

---
+++ NUnitLearning/Registration/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿namespace Registration;
+
+[TestFixture]
+public class Tests
+{
+    private UserRegistration userRegistration;
+    [SetUp]
+    public void Setup()
+    {
+        userRegistration = new UserRegistration();
+    }
+
+    [Test]
+    public void RegisterUser_WithValidInputs()
+    {
+        Assert.DoesNotThrow(() =>
+            {
+                userRegistration.RegisterUser("jaykant", "jaykant@test.com", "secret1");
+            });
+    }
+
+    [TestCase("")]
+        public void RegisterUser_WithInvalidUsername(string username)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                userRegistration.RegisterUser(username, "user@test.com", "secret1");
+            });
+        }
+
+     [TestCase("")]
+        [TestCase("invalidemail")]
+        [TestCase("user.com")]
+        public void RegisterUser_WithInvalidEmail(string email)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                userRegistration.RegisterUser("user1", email, "secret1");
+            });
+        }
+
+        [TestCase("")]
+        [TestCase("123")]
+        [TestCase("abc")]
+        public void RegisterUser_WithInvalidPassword(string password)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                userRegistration.RegisterUser("user1", "user@test.com", password);
+            });
+        }   
+}
---
+++ NUnitLearning/Registration/UserRegistration.cs
@@ -0,0 +1,29 @@
+﻿namespace Registration;
+
+public class UserRegistration
+{
+    public void RegisterUser(string username, string email, string password)
+    {
+            
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            throw new ArgumentException("Username is required.");
+
+        }
+            
+           
+        if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+        {
+            throw new ArgumentException("Invalid email.");
+        }
+            
+
+           
+        if (string.IsNullOrWhiteSpace(password) || password.Length < 6)
+        {
+            throw new ArgumentException("Password must be at least 6 characters long.");
+        }
+            
+
+    }   
+}
