d66ffa65e8e27fa74d83a5087036eb24e48b8a95
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 21:34:07 +0530

    [Jaykant] Add .Date Format NUnit Testing

---
+++ NUnitLearning/Date/DateFormatter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+namespace Date;
+
+public class DateFormatter
+{
+    public string FormatDate(string inputDate)
+    {
+        if (string.IsNullOrWhiteSpace(inputDate))
+        {
+            throw new FormatException("Input date is null or empty.");
+        }
+
+        if (!DateTime.TryParseExact(
+                    inputDate,
+                    "yyyy-MM-dd",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out DateTime date))
+            {
+                throw new FormatException("Invalid date format. Expected yyyy-MM-dd.");
+            }
+            
+            return date.ToString("dd-MM-yyyy");
+    }
+}
---
+++ NUnitLearning/Date/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace Date;
+
+public class Tests
+{
+    private DateFormatter dateFormatter;
+
+    [SetUp]
+    public void Setup()
+    {
+        dateFormatter = new DateFormatter();
+    }
+
+    [TestCase("2026-01-20", "20-01-2026")]
+    [TestCase("1990-12-05", "05-12-1990")]
+    [TestCase("2000-02-29", "29-02-2000")]
+    public void FormatDate_WithValidDate(string input, string expected)
+    {
+        string result = dateFormatter.FormatDate(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+
+    [TestCase("2026/01/20")]  
+    [TestCase("20-01-2026")]
+    [TestCase("2026-13-01")]  
+    [TestCase("2026-02-30")]  
+    [TestCase("")]           
+    public void FormatDate_WithInvalidDate(string input)
+    {
+        Assert.Throws<FormatException>(() =>
+        {
+            dateFormatter.FormatDate(input);
+        });
+    }
+}
