bd5fe9f2be038c873be99b5cdd3082cb9a5dbed6
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 12:54:54 +0530

    [Jaykant] Add .String Utils

---
+++ NUnitLearning/StringUtility/StringUtilityTests.cs
@@ -0,0 +1,40 @@
+﻿namespace StringUtility;
+
+[TestFixture]
+public class StringUtilityTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Reverse_InputString_ReturnsReversed()
+    {
+        string input = "hello";
+
+        string result = StringUtils.Reverse(input);
+
+        Assert.AreEqual("olleh", result);
+    }
+
+    [TestCase("madam", true)]
+    [TestCase("racecar", true)]
+    [TestCase("hello", false)]
+    public void IsPalindrome_ChecksCorrectly(string input, bool expected)
+    {
+        bool result = StringUtils.IsPalindrome(input);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void ToUpperCase_InputString_ReturnsUpperCase()
+    {
+        string input = "hello world";
+
+        string result = StringUtils.ToUpperCase(input);
+
+        Assert.AreEqual("HELLO WORLD", result);
+    }
+}
---
+++ NUnitLearning/StringUtility/StringUtils.cs
@@ -0,0 +1,25 @@
+﻿namespace StringUtility;
+
+public class StringUtils
+{
+    public static string Reverse(string str)
+    {
+        if (str == null) return null;
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public static bool IsPalindrome(string str)
+    {
+        if (str == null) return false;
+        string reversed = Reverse(str);
+        return str == reversed;
+    }
+
+    public static string ToUpperCase(string str)
+    {
+        if (str == null) return null;
+        return str.ToUpper();
+    }
+}
