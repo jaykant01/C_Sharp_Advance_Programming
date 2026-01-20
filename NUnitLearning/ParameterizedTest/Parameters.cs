bbe3b0d785685810518c6086077f2c97fca32e58
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 18:54:02 +0530

    [Jaykant] Add .Check EvenNumber NUnit Testing

---
+++ NUnitLearning/ParameterizedTest/Parameters.cs
@@ -0,0 +1,9 @@
+﻿namespace ParameterizedTest;
+
+public class Parameters
+{
+    public bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+}
---
+++ NUnitLearning/ParameterizedTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿namespace ParameterizedTest;
+
+[TestFixture]
+public class Tests
+{
+    private Parameters parameters;
+    [SetUp]
+    public void Setup()
+    {
+        parameters = new Parameters();
+    }
+
+    [TestCase(2, true)]
+    [TestCase(4, true)]
+    [TestCase(6, true)]
+    [TestCase(7, false)]
+    [TestCase(9, false)]
+    public void IsEven(int number, bool expected)
+    {
+        bool result = parameters.IsEven(number);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+}
