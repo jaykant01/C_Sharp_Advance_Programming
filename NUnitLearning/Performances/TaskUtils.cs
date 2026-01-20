a135b3069215295ae4a9233e7a9e00e25a488e3f
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 19:05:56 +0530

    [Jaykant] Add .Performance NUnit Testing

---
+++ NUnitLearning/Performances/TaskUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace Performances;
+
+public class TaskUtils
+{
+    public string LongRunningTask()
+        {
+            Thread.Sleep(1000);
+            return "Completed";
+        }
+}
---
+++ NUnitLearning/Performances/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace Performances;
+
+public class PerformancesTests
+{
+    private TaskUtils taskUtils;
+    [SetUp]
+    public void Setup()
+    {
+        taskUtils = new TaskUtils();
+    }
+
+    [Test]
+     [Timeout(2000)]
+    public void Test1()
+    {
+        string result = taskUtils.LongRunningTask();
+
+        Assert.That(result, Is.EqualTo("Completed"));
+    }
+}
