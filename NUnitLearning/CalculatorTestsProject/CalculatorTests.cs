fb2e37dff13fca822d601dab96fa638ab28e870a
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 11:44:24 +0530

    [Jaykant] Add .Calculator test

---
+++ NUnitLearning/CalculatorTestsProject/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+namespace CalculatorTestsProject;
+
+[TestFixture]
+public class CalculatorTests
+{
+    private Calculator calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        calculator = new Calculator();
+    }
+
+    [Test]
+    public void Add_TwoNumbers_ReturnSum()
+    {
+        int result = calculator.Add(10, 5);
+        Assert.AreEqual(15, result);
+    }
+
+    [Test]
+    public void Subtract_TwoNumbers_ReturnsDifference()
+    {
+        int result = calculator.Subtract(10, 5);
+        Assert.AreEqual(5, result);
+    }
+
+    [Test]
+    public void Multiply_TwoNumbers_ReturnsProduct()
+    {
+        int result = calculator.Multiply(10, 5);
+        Assert.AreEqual(50, result);
+    }
+
+    [Test]
+    public void Divide_TwoNumbers_ReturnsQuotient()
+    {
+        int result = calculator.Divide(10, 5);
+        Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void Divide_ByZero_ThrowsException()
+    {
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            calculator.Divide(10, 0);
+        });
+    }
+
+}
