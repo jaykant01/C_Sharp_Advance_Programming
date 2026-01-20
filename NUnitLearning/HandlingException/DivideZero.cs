9dab4a9f62a5d0028ce8e61bbe8ca61d3e494904
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 16:21:53 +0530

    [Jaykant] Add .Exception Handling NUnit Testing

---
+++ NUnitLearning/HandlingException/DivideZero.cs
@@ -0,0 +1,13 @@
+﻿namespace HandlingException;
+public class DivideZero
+{
+    public int Divide(int a, int b)
+    {
+        if(b == 0)
+        {
+            throw new System.ArithmeticException("Cannot Divide by zero.");
+        }
+
+        return a/b;
+    }
+}
---
+++ NUnitLearning/HandlingException/HandlingExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿namespace HandlingException;
+
+[TestFixture]
+public class HandlingExceptionTests
+{   
+    private DivideZero divideZero;
+
+    [SetUp]
+    public void Setup()
+    {
+        divideZero = new DivideZero();
+    }
+
+    [Test]
+    public void DivideZero()
+    {
+        // Assert & Act
+        Assert.Throws<ArithmeticException>(() => divideZero.Divide(10, 0));
+    }
+}
