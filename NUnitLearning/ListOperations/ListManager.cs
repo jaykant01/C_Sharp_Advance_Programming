539171e329647f45deb666491f160453a3170818
jaykant01 <jaykanty24092004@gmail.com>
2026-01-20 15:45:39 +0530

    [Jaykant] Add .List Manager NUnit Testing

---
+++ NUnitLearning/ListOperations/ListManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+namespace ListOperations;
+
+public class ListManager
+{
+    public List<int> AddElement(List<int> list, int element)
+    {
+        list.Add(element);
+        return list;
+    }
+
+    public List<int> RemoveElement(List<int> list, int element)
+    {
+        list.Remove(element);
+        return list;
+    }
+
+    public int GetSize(List<int> list)
+    {
+        return list.Count;
+    }
+}
---
+++ NUnitLearning/ListOperations/ListOperationsTests.cs
@@ -0,0 +1,50 @@
+﻿namespace ListOperations;
+
+[TestFixture]
+public class ListOperationsTests
+{
+    private ListManager listManager;
+
+    [SetUp]
+    public void Setup()
+    {
+        listManager = new ListManager();
+    }
+
+    [Test]
+    public void AddElement()
+    {
+        // Arrange
+        var list = new List<int> {1, 3};
+
+        // Act
+        var result = listManager.AddElement(list, 2);
+
+        // Assert
+        Assert.That(result.Count, Is.EqualTo(3));
+        Assert.That(result, Does.Contain(2));
+    }
+
+    [Test]
+    public void RemoveElement()
+    {
+        // Arrange
+        var list = new List<int> {1, 2, 3};
+        // Act
+        var result = listManager.RemoveElement(list, 2);
+        // Assert
+        Assert.That(result.Count, Is.EqualTo(2));
+        Assert.That(result, Does.Not.Contain(2));
+    }
+
+    [Test]
+    public void GetSize()
+    {
+        var list = new List<int> {10, 20, 30};
+
+        int size = listManager.GetSize(list);
+
+        Assert.That(size, Is.EqualTo(3));
+    }
+
+}
